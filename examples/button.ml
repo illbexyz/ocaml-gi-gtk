3b97dd7200a74346ac06bf013bbf93597760bb6f
Alberto Nicoletti <albyx.n@gmail.com>
2020-01-22 22:10:20 +0100

    Updated buttons example

--- examples/button.ml
+++ examples/button.ml
@@ -1,17 +1,22 @@
 let main () =
   let _ = GMain.init () in
-  let window = GWindow.window ~title:"Pixmap'd Buttons!" ~border_width:10 () in
+  let window = GWindow.window ~title:"Buttons" ~border_width:10 () in
   let _ = window#connect#destroy ~callback:GMain.quit in
   let hbox = GPack.hbox ~packing:window#add () in
   let button = GIGtk.ButtonG.button ~packing:(hbox#pack ~padding:5) () in
+  let _ = button#set_label "One" in
   let _ = button#connect#clicked ~callback:
-      (fun () -> prerr_endline "Hello again - cool button was pressed") in
-  let button = GIGtk.ButtonG.button ~use_underline:true ~label:"_Coucou" ~packing:(hbox#pack ~padding:5) () in
+      (fun () -> prerr_endline "Button one clicked") in
+  let button = GIGtk.ButtonG.button ~use_underline:true ~label:"Two" ~packing:(hbox#pack ~padding:5) () in
   let _ = button#connect#clicked ~callback:
-      (fun () -> prerr_endline "Coucou") in
-  let button = GIGtk.ButtonG.button ~packing:(hbox#pack ~padding:5) () in
+      (fun () ->
+        let () = prerr_endline "Button two clicked, GC" in
+        Gc.compact ()
+      ) in
+  let button = GIGtk.ButtonG.button ~use_stock:true ~packing:(hbox#pack ~padding:5) () in
+  let _ = button#set_label "HOME" in
   let _ = button#connect#clicked ~callback:
-      (fun () -> prerr_endline "Stock buttons look nice") in
+      (fun () -> prerr_endline "HOME button clicked") in
   let _ = window#show () in
   GMain.main ()
 
