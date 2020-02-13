1e2ba762d56ad3010f34e56a994869d066a637d2
illbexyz <albyx.n@gmail.com>
2020-02-13 17:09:34 +0100

    Updated the generate.sh script

--- generate.sh
+++ generate.sh
@@ -3,56 +3,90 @@
 install=false
 full=false
 
-while getopts ":if" opt; do
+# The order is important because some libs depends on the previous ones
+# and they will throw errors if they are not installed in the right order
+avail_libs="glib gobject gio atk cairo pango gdkpixbuf gdk gtk"
+
+lib_name_to_dir () {
+  local lib_name=$1
+  case "$lib_name" in
+    glib      ) echo "GLib";;
+    gobject   ) echo "GObject";;
+    gio       ) echo "Gio";;
+    atk       ) echo "Atk";;
+    cairo     ) echo "cairo";;
+    pango     ) echo "Pango";;
+    gdkpixbuf ) echo "GdkPixbuf";;
+    gdk       ) echo "Gdk";;
+    gtk       ) echo "Gtk";;
+    *         )
+      echo "Error: unknown lib name, exiting"
+      exit 1
+      ;;
+  esac
+}
+
+usage () {
+  echo "Usage: generate.sh [OPTION] LIB [LIB...]"
+  echo ""
+  echo "Available libs are: $avail_libs"
+  echo ""
+  echo "Options:"
+  echo "  -h, (help)     Display this help and exit"
+  echo "  -f, (full)     Generate every available library. The LIB arguments will be ignored."
+  echo "  -i, (install)  After the library generation, it will install it using 'dune install'"
+  exit 0
+}
+
+while getopts ":ifh" opt; do
   case ${opt} in
-    i ) # process option a
+    i )
       install=true
       ;;
     f )
       full=true
       ;;
-    \? ) echo "Usage: generate.sh [-i] (install), [-f] (full)"
+    h ) usage
       ;;
   esac
 done
 
+shift $((OPTIND - 1))
+lib=$1
 
 if $full; then
-    libs="gobject glib gio atk pango cairo gdk gdkpixbuf gtk"
+    libs=$avail_libs
 else
-    libs="gtk"
+    libs=$lib
+fi
+
+# When no lib nor the full argument are specified
+if [ -z "$libs" ]; then
+  usage
 fi
 
 # From: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 export BASE_OCAML_C=$DIR/base-ocaml/c
-export GI_INCLUDES="-I$DIR/bindings/GObject/include -I$DIR/bindings/GLib/include -I$DIR/bindings/Gio/include -I$DIR/bindings/Atk/include -I$DIR/bindings/GdkPixbuf/include -I$DIR/bindings/Gdk/include -I$DIR/bindings/Pango/include -I$DIR/bindings/cairo/include"
 
-generate () {
-    stack build && stack exec ocaml-gi-gtk-exe $libs
+include_dirs () {
+  for avail_lib in $avail_libs; do
+    local lib_dir=$(lib_name_to_dir $avail_lib)
+    echo "-I$DIR/bindings/$lib_dir/include"
+  done
 }
 
-build () {
-    local lib_name=$1
-    pushd bindings/$lib_name > /dev/null
-        dune build
-        if $install; then
-            dune install
-        fi
-    popd > /dev/null
+export GI_INCLUDES=$(include_dirs)
+
+install () {
+  local lib_name=$1
+  local lib_dir=$(lib_name_to_dir $lib_name)
+  dune install --root bindings/$lib_dir
 }
 
-generate
-
-# if $full; then
-#     build GObject
-#     build GLib
-#     build Gio
-#     build Atk
-#     build GdkPixbuf
-#     build Gdk
-#     build Pango
-# fi
-
-# build Gtk
+for lib in $libs; do
+  stack build && stack exec ocaml-gi-gtk-exe $lib
+  if $install; then
+    install $lib
+  fi
+done
