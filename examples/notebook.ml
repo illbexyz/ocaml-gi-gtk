b5b1db7f36acb706e36b5665d5db5746cdc6352f
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 15:51:37 +0100

    notebook example ported

--- examples/notebook.ml
+++ examples/notebook.ml
@@ -1,25 +1,39 @@
+(**************************************************************************)
+(*    Lablgtk - Examples                                                  *)
+(*                                                                        *)
+(*    This code is in the public domain.                                  *)
+(*    You may freely copy parts of it in your application.                *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* $Id$ *)
+
 open GIGtk
 
 let main () =
-  let _ = GMain.init () in
-
-  let window = WindowG.window ~show:false () in
-  window#set_title "Notebook";
-  window#set_border_width 10;
-  ignore @@ window#connect#destroy ~callback:GMain.quit;
-
+  GMain.init ();
+  let window = WindowG.window ~title:"Notebook" ~border_width:10 () in
+  window#connect#destroy ~callback:GMain.quit;
   let notebook = NotebookG.notebook ~packing:window#add () in
-  let button1 = ButtonG.button ~label:"Page 1" ~packing:(fun w -> ignore (notebook#append_page w None)) () in
-  ignore @@ button1#connect#clicked ~callback:(fun () -> prerr_endline "Hello again - cool button 1 was pressed");
+  let button = ButtonG.button ~label:"Page 1" 
+    ~packing:(fun w -> ignore (notebook#append_page w None)) () in
+  button#connect#clicked ~callback:
+    (fun () -> prerr_endline "Hello again - cool button 1 was pressed");
 
-  let button2 = ButtonG.button ~label:"Page 2" ~packing:(fun w -> ignore (notebook#append_page w None)) () in
-  ignore @@ button2#connect#clicked ~callback:(fun () -> prerr_endline "Hello again - cool button 1 was pressed");
-
-  ignore @@ notebook#connect#switch_page ~callback:(fun _ i -> prerr_endline ("Page switch to " ^ string_of_int i));
-
-  ignore @@ button2#connect#clicked ~callback:(fun () -> prerr_endline "Coucou");
-
+  let button = ButtonG.button ~label:"Page 2" 
+   ~packing:(fun w -> ignore (notebook#append_page w None))
+    () in
+  button#connect#clicked ~callback:
+    (fun () -> prerr_endline "Hello again - cool button 2 was pressed");
+  notebook#connect#switch_page 
+    ~callback:(fun _ i -> prerr_endline ("Page switch to " ^ string_of_int i));
+  button#connect#clicked ~callback:
+    (fun () -> prerr_endline "Coucou");
   window#misc#show ();
   GMain.main ()
 
 let _ = main ()
+
+(* Local Variables: *)
+(* compile-command: "ocamlc -I ../src -w s lablgtk.cma notebook.ml" *)
+(* End: *)
