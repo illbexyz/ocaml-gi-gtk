c06efd4b0533bfc7b60a9fbe67223be9ca2c1953
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 22:05:50 +0100

    progressbar example ported

--- examples/progressbar.ml
+++ examples/progressbar.ml
@@ -8,35 +8,37 @@
 
 (* $Id$ *)
 
+open GIGtk
+
 let main () =
 
   GMain.init ();
-  let window = GWindow.window ~border_width: 10 () in
+  let window = WindowG.window ~border_width: 10 () in
   window#connect#destroy ~callback:GMain.quit;
 
-  let table = GPack.table ~rows:3 ~columns:2 ~packing: window#add () in
+  let table = TableG.table ~n_rows:3 ~n_columns:2 ~packing: window#add () in
   
-  GMisc.label ~text:"Progress Bar Example" ()
-    ~packing:(table#attach ~left:0 ~right:2 ~top:0 ~expand:`X ~shrink:`BOTH);
+  LabelG.label ~label:"Progress Bar Example" ()
+    ~packing:(Lablgtk3Compat.attach table ~left:0 ~right:2 ~top:0 ~expand:`X ~shrink:`BOTH);
   
   let pbar =
-    GRange.progress_bar ~pulse_step:0.01 ()
-      ~packing:(table#attach ~left:0 ~right:2 ~top:1
+    ProgressBarG.progress_bar ~pulse_step:0.01 ()
+      ~packing:(Lablgtk3Compat.attach table ~left:0 ~right:2 ~top:1
                   ~expand:`BOTH ~fill:`X ~shrink:`BOTH) in
 
-  let ptimer = GMain.Timeout.add ~ms:50 ~callback:(fun () -> pbar#pulse(); true) in
+  let ptimer = GMain.Timeout.add ~ms:50 ~callback:(fun () -> pbar#pulse; true) in
 
-  let button = GButton.button ~label:"Reset" ()
-      ~packing:(table#attach ~left:0 ~top:2
+  let button = ButtonG.button ~label:"Reset" ()
+      ~packing:(Lablgtk3Compat.attach table ~left:0 ~top:2
                   ~expand:`NONE ~fill:`X ~shrink:`BOTH) in
   button#connect#clicked ~callback:(fun () -> pbar#set_fraction 0.);
 
-  let button = GButton.button ~label:"Cancel" ()
-      ~packing:(table#attach ~left:1 ~top:2
+  let button = ButtonG.button ~label:"Cancel" ()
+      ~packing:(Lablgtk3Compat.attach table ~left:1 ~top:2
                   ~expand:`NONE ~fill:`X ~shrink:`BOTH) in
   button#connect#clicked ~callback:GMain.quit;
 
-  window#show ();
+  window#misc#show ();
   GMain.main ()
 
 let _ = main ()
