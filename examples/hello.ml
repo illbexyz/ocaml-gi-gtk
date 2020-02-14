ea4dc9a237235aec0df361b945a115b3fb1de5c0
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 22:11:38 +0100

    hello example ported bugged

--- examples/hello.ml
+++ examples/hello.ml
@@ -8,19 +8,21 @@
 
 (* $Id$ *)
 
+open GIGtk
+
 let _ = GMain.init ()
 
-let window = GWindow.window ~border_width: 10 ()
+let window = WindowG.window ~border_width: 10 ()
 
-let button = GButton.button ~label:"Hello World" ~packing: window#add ()
+let button = ButtonG.button ~label:"Hello World" ~packing: window#add ()
 
 let main () =
-  window#event#connect#delete 
-    ~callback:(fun _ -> prerr_endline "Delete event occured"; true);
+  (*XXX window#event#connect#delete 
+    ~callback:(fun _ -> prerr_endline "Delete event occured"; true);*)
   window#connect#destroy ~callback:GMain.quit;
   button#connect#clicked ~callback:(fun () -> prerr_endline "Hello World");
   button#connect#clicked ~callback:window#destroy;
-  window#show ();
+  window#misc#show ();
   GMain.main ()
 
 let _ = Printexc.print main ()
