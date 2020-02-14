93b6eadc7544c06534174b6b9835d651516d21bd
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 15:05:05 +0100

    calendar ported (bug found)

--- examples/calendar.ml
+++ examples/calendar.ml
@@ -1,18 +1,30 @@
+(**************************************************************************)
+(*    Lablgtk - Examples                                                  *)
+(*                                                                        *)
+(*    This code is in the public domain.                                  *)
+(*    You may freely copy parts of it in your application.                *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* $Id$ *)
+
 open GIGtk
 
 let main () =
-  let _ = GMain.init () in
-
+  GMain.init ();
   let window = WindowG.window () in
-  ignore @@ window#connect#destroy ~callback:GMain.quit;
+  window#connect#destroy ~callback:GMain.quit;
 
   let calendar = CalendarG.calendar ~packing:window#add () in
-  ignore @@ calendar#connect#day_selected ~callback:(fun () ->
-      let (_, year,month,day) = calendar#get_date in
-      Printf.printf "You selected %d/%d/%02d.\n" day (month+1) (year mod 100);
+  calendar#connect#day_selected ~callback:
+    begin fun () ->
+      let ((),year,month,day) = calendar#get_date in
+      Printf.printf "You selected %d/%d/%02d.\n"
+	day (month+1) (year mod 100);
       flush stdout
-    );
+    end;
 
-  GMain.main()
+  window#misc#show ();
+  GMain.main ()
 
 let _ = main ()
