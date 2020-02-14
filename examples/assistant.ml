5ba7c50c29b35c036d4f1a0700ec60d065afd371
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 22:58:15 +0100

    assistant example ported

--- examples/assistant.ml
+++ examples/assistant.ml
@@ -7,26 +7,29 @@
 (**************************************************************************)
 
 (* $Id: $ *)
+
+open GIGtk
+
 let main () =
   GMain.init ();
 
-  let assistant = GAssistant.assistant () in
+  let assistant = AssistantG.assistant () in
 
-  let box = GPack.vbox () in
-  ignore (assistant#append_page box#as_widget);
-  assistant#set_page_complete box#as_widget true;
+  let box = VBoxG.v_box () in
+  ignore (assistant#append_page box);
+  assistant#set_page_complete box true;
   prerr_endline "Complete";
-  assistant#set_page_type box#as_widget `SUMMARY;
-  let button = GButton.link_button 
-    "http://HELLO.ORG" 
+  assistant#set_page_type box `SUMMARY;
+  let button = LinkButtonG.link_button 
+    ~uri:"http://HELLO.ORG" 
     ~label:"BYE" ~packing:box#add () 
   in
   button#set_uri "GHHHHH";
   Format.printf "Got:%a@." GUtil.print_widget button;
   button#connect#activate_link
-    (fun () -> Format.printf "Got url '%s'@." button#uri;   button#set_uri "AGAIN");
+    (fun () -> Format.printf "Got url '%s'@." button#get_uri;   button#set_uri "AGAIN");
   assistant#connect#close GMain.quit;
-  assistant#show ();
+  assistant#misc#show ();
   GMain.main ()
 
 let _ = main ()
