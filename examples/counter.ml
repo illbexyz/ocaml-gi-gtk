2427316a2a2302ef3c7f26e3e5b6abc564ccf4de
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-14 15:46:15 +0100

    counter example roughly ported

--- examples/counter.ml
+++ examples/counter.ml
@@ -1,32 +1,46 @@
+(**************************************************************************)
+(*    Lablgtk - Examples                                                  *)
+(*                                                                        *)
+(*    This code is in the public domain.                                  *)
+(*    You may freely copy parts of it in your application.                *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* $Id$ *)
+
 open GIGtk
 
-let _ = GMain.init ()
+let _ = GMain.init()
 
-let w = WindowG.window ~show:true ()
+let w = WindowG.window ()
 
 let vb = VBoxG.v_box ~packing:w#add ()
 
 let lbl = LabelG.label ~packing:vb#add ()
 
 let hb = HBoxG.h_box ~packing:vb#add ()
-
 let decB = ButtonG.button ~label:"Dec" ~packing:hb#add ()
-
 let incB = ButtonG.button ~label:"Inc" ~packing:hb#add ()
 
-let adj = AdjustmentG.adjustment ~lower:0. ~upper:100. ~step_increment:1. ~page_increment:10. ()
+let adj =
+  GData.adjustment ~lower:0. ~upper:100. ~step_incr:1. ~page_incr:10. ()
 
-let sc = ScaleG.scale (*~adjustment:adj#as_adjustment*) ~packing:vb#add ()
+let sc = HScaleG.h_scale ~adjustment:(adj#as_adjustment) ~draw_value:false
+    ~packing:vb#add ()
 
-let counter = ref 0
+let counter = new GUtil.variable 0
 
 let _ =
-  ignore @@ decB#connect#clicked ~callback:(fun () -> adj#set_value (float(!counter-1)));
-  ignore @@ incB#connect#clicked ~callback:(fun () -> adj#set_value (float(!counter+1)));
-
-  ignore @@ sc#connect#change_value ~callback:(fun _ v -> Printf.printf "drag: %i\n%!" (truncate v));
-  ignore @@ adj#connect#value_changed ~callback:(fun () -> counter := truncate (adj#get_value));
-
-  ignore @@ w#connect#destroy ~callback:GMain.quit;
+  decB#connect#clicked
+    ~callback:(fun () -> adj#set_value (float(counter#get-1)));
+  incB#connect#clicked
+    ~callback:(fun () -> adj#set_value (float(counter#get+1)));
+  sc#connect#change_value
+    ~callback:(fun _ v -> Printf.printf "drag: %i\n%!" (truncate v));
+  adj#connect#value_changed
+    ~callback:(fun () -> counter#set (truncate adj#value));
+  counter#connect#changed ~callback:(fun n -> lbl#set_text (string_of_int n));
+  counter#set 0;
+  w#connect#destroy ~callback:GMain.quit;
   w#misc#show ();
   GMain.main ()
