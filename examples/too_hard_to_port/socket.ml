eb5bfad058c301de1e715d958ec0e3fcff61eb1f
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-17 02:36:11 +0100

    socket example is doing nasty things

--- examples/too_hard_to_port/socket.ml
+++ examples/too_hard_to_port/socket.ml
@@ -8,16 +8,18 @@
 
 (* $Id$ *)
 
+open GIGtk
+
 let main () =
   GMain.init ();
-  let w = GWindow.window ~title:"Socket example" () in
+  let w = WindowG.window ~title:"Socket example" () in
   w#connect#destroy ~callback:GMain.quit;
-  let vbox = GPack.vbox ~packing:w#add () in
-  let label = GMisc.label ~packing:vbox#pack () in
-  w#show ();
-  let socket = GWindow.socket ~packing:vbox#add ~height:40 () in
+  let vbox = VBoxG.v_box ~packing:w#add () in
+  let label = LabelG.label ~packing:(Lablgtk3Compat.pack vbox) () in
+  w#misc#show ();
+  let socket = SocketG.socket ~packing:vbox#add ~height:40 () in
   label#set_text ("XID to plug into this socket: 0x" ^ 
-                  Int32.format "%x" socket#xwindow);
+                  Int32.format "%x" socket#get_xid);
   GMain.main ()
 
 let _ = main ()
