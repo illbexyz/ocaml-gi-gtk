d5c7fc5f5b395047d5f1171f877cb113d1b8f2ff
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-17 10:08:59 +0100

    entry2 example ported bugged

--- examples/entry2.ml
+++ examples/entry2.ml
@@ -6,14 +6,18 @@
 (*                                                                        *)
 (**************************************************************************)
 
+open GIGtk
+
 let _ = GMain.init ()
-let w = GWindow.window ~show:true ()
-let e = GEdit.entry ~packing:w#add ()
+let w = WindowG.window ~show:true ()
+let e = EntryG.entry ~packing:w#add ()
 
 let () =
+(*XXX
   e#connect#after#insert_text
     (fun _ ~pos ->
       if e#text_length > 5 then e#set_secondary_icon_stock `DIALOG_WARNING
       else e#set_secondary_icon_name "");
+*)
   w#event#connect#delete (fun _ -> GMain.quit (); true);
   GMain.main ()
