c39ae5863701796cb22beff5db618ef29c53715f
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2020-02-17 10:19:35 +0100

    signal_override ported

--- examples/signal_override.ml
+++ examples/signal_override.ml
@@ -6,6 +6,8 @@
 (*                                                                        *)
 (**************************************************************************)
 
+open GIGtk
+
 module C = Gobject.Closure
 
 let add_closure argv =
@@ -21,27 +23,31 @@ let derived_frame_gtype =
   lazy begin
     let parent = Gobject.Type.from_name "GtkFrame" in
     let t = Gobject.Type.register_static ~parent ~name:derived_frame_name in
-    GtkSignal.override_class_closure GtkContainers.Container.S.add t
+    GtkSignal.override_class_closure Container.S.add t
       (C.create add_closure) ;
     t
   end
 
+let pack_container ~create =
+  Container.make_params ~cont:
+    (fun p ?packing ?show () -> GObj.pack_return (create p) ~packing ~show)
+
 let create_derived_frame =
-  GtkBin.Frame.make_params [] 
+  Frame.make_params [] 
     ~cont:(fun pl -> 
-      GContainer.pack_container pl 
+      pack_container pl 
 	~create:(fun pl -> 
 	  ignore (Lazy.force derived_frame_gtype) ;
-	  new GBin.frame (GtkObject.make derived_frame_name pl : Gtk.frame Gtk.obj)))
+	  new FrameG.frame (GtkObject.make derived_frame_name pl)))
 
 let main =
   GMain.init ();
-  let w = GWindow.window ~title:"Overriding signals demo" () in
+  let w = WindowG.window ~title:"Overriding signals demo" () in
   w#connect#destroy GMain.quit ;
 
   let f = create_derived_frame ~label:"Talking frame" ~packing:w#add () in
 
-  let l = GMisc.label ~markup:"This is the <b>GtkFrame</b>'s content" ~packing:f#add () in
+  let l = LabelG.label ~use_markup:true ~label:"This is the <b>GtkFrame</b>'s content" ~packing:f#add () in
 
-  w#show () ;
+  w#misc#show () ;
   GMain.main ()
